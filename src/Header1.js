b565d495e780d254df6022a833b1844f02936125
npx <chandrikajanjhyam@gmail.com>
2024-03-06 18:27:05 +0530

    modified file

---
+++ src/Header1.js
@@ -0,0 +1,8 @@
+function Header1() {
+    return (
+        <>
+          <div className="head"><h1>Fruits Shopee</h1></div>
+          <marquee>Health is Wealth</marquee>
+        </>  
+)}
+export default Header1
